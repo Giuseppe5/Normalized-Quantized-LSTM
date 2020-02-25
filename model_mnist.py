8b99be11b3443ffe8b1ee2148b6130b288f51aaa
Giuseppe <giuseppefranco4@gmail.com>
2020-02-25 16:26:53 +0000

    Added TensorBatchNorm

--- model_mnist.py
+++ model_mnist.py
@@ -1,11 +1,12 @@
 """Implementation of batch-normalized LSTM."""
 import torch
 from torch import nn
-from torch.nn import   Parameter
+from torch.nn import Parameter
 import torch.nn.functional as F
 import numpy as np
-class SeparatedBatchNorm1d(nn.Module):
 
+
+class SeparatedBatchNorm1d(nn.Module):
     """
     A batch normalization module which keeps its running mean
     and variance separately per timestep.
@@ -69,8 +70,6 @@ class SeparatedBatchNorm1d(nn.Module):
                 .format(name=self.__class__.__name__, **self.__dict__))
 
 
-
-
 def _norm(p, dim):
     """Computes the norm over all dimensions except dim"""
     if dim is None:
@@ -83,6 +82,7 @@ def _norm(p, dim):
         return p.contiguous().view(-1, p.size(-1)).norm(dim=0).view(*output_size)
     else:
         return _norm(p.transpose(0, dim), 0).transpose(0, dim)
+
 
 class WeightNorm(nn.Module):
 
@@ -98,9 +98,42 @@ class WeightNorm(nn.Module):
         return self.g * v
 
 
+class TensorBatchNorm(nn.Module):
+    def __init__(self, eps=1e-5, momentum=0.1):
+        self.register_buffer('running_mean', torch.zeros(1))
+        self.register_buffer('running_var', torch.ones(1))
+        self.eps = eps
+        self.momentum = momentum
+        self.weight = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+        self.running_mean.zero_()
+        self.running_var.fill_(1)
+
+    def forward(self, input: torch.Tensor):
+        if self.training:
+            mean = input.mean()
+            unbias_var = input.var(unbiased=True)
+            self.running_mean = (1 - self.momentum) * self.running_mean + mean.detach() * self.momentum
+            self.running_var = (1 - self.momentum) * self.running_var + unbias_var.detach() * self.momentum
+            biased_var = input.var(unbiased=False)
+            output = functional_tensor_batch_norm(input, mean, biased_var, self.eps, self.weight)
+            # inv_std = 1/(biased_var + self.eps).pow(0.5)
+            # output = (input-mean) * inv_std * self.weight
+            return output
+        else:
+            output = functional_tensor_batch_norm(input, self.running_mean, self.running_var, self.eps, self.weight)
+            return output
+
+
+def functional_tensor_batch_norm(input: torch.Tensor, mean: torch.Tensor, var: torch.Tensor, eps: float,
+                                 weight: torch.Tensor):
+    inv_std = 1 / (var + eps).pow(0.5)
+    output = (input - mean) * inv_std * weight
+    return output
+
+
 class LSTM_quantized_cell(nn.Module):
 
-    def __init__(self, input_size, hidden_size,norm,args, bias=True):
+    def __init__(self, input_size, hidden_size, norm, args, bias=True):
         super(LSTM_quantized_cell, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -141,10 +174,10 @@ class LSTM_quantized_cell(nn.Module):
             self.layernorm_ohh = nn.LayerNorm(hidden_size)
             self.layernorm_ct = nn.LayerNorm(hidden_size)
             self.reset_parameters()
-            self.reset_LN_parameters() # g=0.1 leanrns faster than g=1
+            self.reset_LN_parameters()  # g=0.1 leanrns faster than g=1
         elif self.norm == 'weight':
             self.reset_parameters()
-            self.weightnorm_ih = WeightNorm(self.weight_ih_l0_full_precision) # this will cause each row becomes 1.
+            self.weightnorm_ih = WeightNorm(self.weight_ih_l0_full_precision)  # this will cause each row becomes 1.
             self.weightnorm_hh = WeightNorm(self.weight_hh_l0_full_precision)
         elif self.norm == 'batch':
             if self.args.shared:
@@ -152,12 +185,15 @@ class LSTM_quantized_cell(nn.Module):
                 self.batchnorm_ih = nn.BatchNorm1d(4 * hidden_size)
                 self.batchnorm_hh = nn.BatchNorm1d(4 * hidden_size)
             else:
-                max_length= 784
+                max_length = 784
                 self.batchnorm_ih = SeparatedBatchNorm1d(num_features=4 * hidden_size, max_length=max_length)
                 self.batchnorm_hh = SeparatedBatchNorm1d(num_features=4 * hidden_size, max_length=max_length)
                 self.batchnorm_c = SeparatedBatchNorm1d(num_features=hidden_size, max_length=max_length)
                 self.reset_parameters()
                 self.reset_BN_parameters()
+        elif self.norm == 'tensorbatch':
+            self.tensorbatchnorm_i, self.tensorbatchnorm_f, self.tensorbatchnorm_a, self.tensorbatchnorm_o = \
+                TensorBatchNorm(), TensorBatchNorm(), TensorBatchNorm(), TensorBatchNorm()
         else:
             self.reset_parameters()
             self.weight_ih_l0.data.copy_(self.weight_ih_l0_full_precision.data)
@@ -173,22 +209,23 @@ class LSTM_quantized_cell(nn.Module):
             q = u if u.shape == flat_shape else v  # pick the one with the correct shape
             q = q.reshape(shape)
             return q[:shape[0], :shape[1]]
+
         tmp = np.concatenate([
             np.eye(self.hidden_size),
             orthogonal((self.hidden_size,
                         3 * self.hidden_size)), ], axis=1).astype(np.float32)
         tmp = np.transpose(tmp)
-        weight_hh = np.zeros((4 * self.hidden_size, self.hidden_size),dtype=np.float32)
+        weight_hh = np.zeros((4 * self.hidden_size, self.hidden_size), dtype=np.float32)
         weight_hh[:2 * self.hidden_size, :] = tmp[self.hidden_size: 3 * self.hidden_size, :]
-        weight_hh[2 * self.hidden_size:3* self.hidden_size, :] = tmp[ :self.hidden_size, :]
-        weight_hh[3* self.hidden_size:, :] = tmp[3* self.hidden_size:, :]
+        weight_hh[2 * self.hidden_size:3 * self.hidden_size, :] = tmp[:self.hidden_size, :]
+        weight_hh[3 * self.hidden_size:, :] = tmp[3 * self.hidden_size:, :]
         weight_hh_data = torch.from_numpy(weight_hh)
         self.weight_hh_l0_full_precision.data.copy_(weight_hh_data)
         tmp = np.transpose(orthogonal((1, 4 * self.hidden_size)))
         weight_ih_data = torch.from_numpy(tmp)
         self.weight_ih_l0_full_precision.data.copy_(weight_ih_data)
         nn.init.constant_(self.bias_ih_l0.data, val=0)
-        self.bias_ih_l0.data[self.hidden_size:2*self.hidden_size,] = 1.
+        self.bias_ih_l0.data[self.hidden_size:2 * self.hidden_size, ] = 1.
 
     def reset_BN_parameters(self):
         if self.norm == 'batch':
@@ -228,16 +265,18 @@ class LSTM_quantized_cell(nn.Module):
             self.weight_ih = self.weightnorm_ih(self.weight_ih_l0)
             self.weight_hh = self.weightnorm_hh(self.weight_hh_l0)
 
-    def forward(self, x, hidden, time):
+    def forward(self, x, hidden, time, first=False):
         # size of x should be like (batch_size, input_dim)
         # if hidden exist, size should be like (batch_size, hidden_dim)
         h, c = hidden
         if self.norm == 'weight':
             pre_ih = F.linear(x, self.weight_ih, )
             pre_hh = F.linear(h, self.weight_hh, )
+            i, f, a, o = torch.split(pre_ih + pre_hh + self.bias_ih_l0, self.hidden_size, dim=1)
         if self.norm == 'layer' or self.norm == 'batch' or not self.norm:
             pre_ih = F.linear(x, self.weight_ih_l0, )
             pre_hh = F.linear(h, self.weight_hh_l0, )
+            i, f, a, o = torch.split(pre_ih + pre_hh + self.bias_ih_l0, self.hidden_size, dim=1)
         if self.norm == 'batch':
             if self.args.shared:
                 pre_ih = self.batchnorm_ih(pre_ih)
@@ -245,15 +284,42 @@ class LSTM_quantized_cell(nn.Module):
             else:
                 pre_ih = self.batchnorm_ih(pre_ih, time=time)
                 pre_hh = self.batchnorm_hh(pre_hh, time=time)
+            i, f, a, o = torch.split(pre_ih + pre_hh + self.bias_ih_l0, self.hidden_size, dim=1)
+        if self.norm == 'tensorbatch':
+            pre_ih = F.linear(x, self.weight_ih, )
+            pre_hh = F.linear(h, self.weight_hh, )
+            ii, fi, ai, oi = torch.split(pre_ih, self.hidden_size, dim=1)
+            ih, fh, ah, oh = torch.split(pre_hh, self.hidden_size, dim=1)
+            ib, fb, ab, ob = torch.split(self.bias_ih_l0, self.hidden_size, dim=0)
+            if not first:
+                i = self.tensorbatchnorm(ii + ih)
+                f = self.tensorbatchnorm(fi + fh)
+                a = self.tensorbatchnorm(ai + ah)
+                o = self.tensorbatchnorm(oi + oh)
+            else:
+                i = functional_tensor_batch_norm(ii+ih, self.layernorm_i.running_mean.detach(),
+                                                      self.layernorm_i.running_var.detach(), self.layernorm_i.eps,
+                                                      self.layernorm_i.weight)
+                f = functional_tensor_batch_norm(fi+fh, self.layernorm_f.running_mean.detach(),
+                                                      self.layernorm_f.running_var.detach(), self.layernorm_f.eps,
+                                                      self.layernorm_f.weight)
+                a = functional_tensor_batch_norm(ai+ah, self.layernorm_a.running_mean.detach().detach(),
+                                                      self.layernorm_a.running_var.detach(), self.layernorm_a.eps,
+                                                      self.layernorm_a.weight)
+                o = functional_tensor_batch_norm(oi+oh, self.layernorm_o.running_mean.detach(),
+                                                      self.layernorm_o.running_var.detach(), self.layernorm_o.eps,
+                                                      self.layernorm_o.weight)
+            i, f, a, o = i+ib, f+fb, a+ab, o+ob
         if self.norm == 'layer':
             ii, fi, ai, oi = torch.split(pre_ih, self.hidden_size, dim=1)
             ih, fh, ah, oh = torch.split(pre_hh, self.hidden_size, dim=1)
             ib, fb, ab, ob = torch.split(self.bias_ih_l0, self.hidden_size, dim=0)
-            ii, fi, ai, oi = self.layernorm_iih(ii), self.layernorm_fih(fi), self.layernorm_aih(ai), self.layernorm_oih(oi)
-            ih, fh, ah, oh = self.layernorm_ihh(ih), self.layernorm_fhh(fh), self.layernorm_ahh(ah), self.layernorm_ohh(oh)
-            i, f, a, o = ii + ih + ib, fi + fh + fb, ah + ai +ab, oh + oi + ob
-        else:
-            i, f, a, o = torch.split(pre_ih + pre_hh + self.bias_ih_l0,self.hidden_size, dim=1)
+            ii, fi, ai, oi = self.layernorm_iih(ii), self.layernorm_fih(fi), self.layernorm_aih(ai), self.layernorm_oih(
+                oi)
+            ih, fh, ah, oh = self.layernorm_ihh(ih), self.layernorm_fhh(fh), self.layernorm_ahh(ah), self.layernorm_ohh(
+                oh)
+            i, f, a, o = ii + ih + ib, fi + fh + fb, ah + ai + ab, oh + oi + ob
+
         i = i.sigmoid()
         f = f.sigmoid()
         a = a.tanh()
@@ -270,7 +336,7 @@ class LSTM_quantized_cell(nn.Module):
             m = tensor.data.norm(p=1).div(tensor.data.nelement())
             quan_tensor = tensor.data.sign().mul(m.expand(s))
         elif method == 'binary_connect':
-            quan_tensor = tensor.data.sign() ## need to change to hard sigmoid
+            quan_tensor = tensor.data.sign()  ## need to change to hard sigmoid
         elif method == 'lab':
             s = tensor.data.size()
             D = acc.sqrt() + 1e-8
@@ -291,20 +357,20 @@ class LSTM_quantized_cell(nn.Module):
             mask = (tensor.abs() > thres).float()
             alpha = (mask * tensor).abs().sum() / mask.sum()
             quan_tensor = alpha * pos - alpha * neg  # use the same scaling for positive and negative weights
-        elif method == 'lat': # approximate loss-aware weight ternarization
+        elif method == 'lat':  # approximate loss-aware weight ternarization
             D = acc.sqrt() + 1e-8
             b = tensor.data.sign()
             # compute the threshold, converge within 10 iterations
             alpha = (b * D * tensor.data).abs().sum() / (b * D).abs().sum()
-            b = (tensor.data > alpha/2).float() - (tensor.data < -alpha/2).float()
-            alpha_old=100.
+            b = (tensor.data > alpha / 2).float() - (tensor.data < -alpha / 2).float()
+            alpha_old = 100.
             while (alpha_old - alpha).abs() > 1e-6:
                 # minimize alpha
                 alpha_old = alpha
                 alpha = (b * D * tensor.data).abs().sum() / (b * D).abs().sum()
                 # minimize b
-                b = (tensor.data > alpha/2).float() - (tensor.data < -alpha/2).float()
-            quan_tensor = alpha*b
+                b = (tensor.data > alpha / 2).float() - (tensor.data < -alpha / 2).float()
+            quan_tensor = alpha * b
         return quan_tensor
 
     def quantize(self, optimizer):
@@ -316,35 +382,42 @@ class LSTM_quantized_cell(nn.Module):
             D_ih_l0 = torch.ones_like(self.weight_ih_l0.data)
             D_hh_l0 = torch.ones_like(self.weight_hh_l0.data)
         method = self.args.method  # currently also supports twn, rowwise_bwn and row-wise_twn
-        if self.weight_ih_l0.shape[1]==1:
-            self.weight_ih_l0.data[:,:] = self.weight_ih_l0_full_precision.data
+        if self.weight_ih_l0.shape[1] == 1:
+            self.weight_ih_l0.data[:, :] = self.weight_ih_l0_full_precision.data
         else:
             self.weight_ih_l0.data[:self.hidden_size, :] = self.Quantization(
                 self.weight_ih_l0_full_precision[:self.hidden_size, :], D_ih_l0[:self.hidden_size, :], method=method)
             self.weight_ih_l0.data[self.hidden_size: 2 * self.hidden_size, :] = self.Quantization(
-                self.weight_ih_l0_full_precision[self.hidden_size: 2 * self.hidden_size, :], D_ih_l0[self.hidden_size: 2 * self.hidden_size, :], method=method)
+                self.weight_ih_l0_full_precision[self.hidden_size: 2 * self.hidden_size, :],
+                D_ih_l0[self.hidden_size: 2 * self.hidden_size, :], method=method)
             self.weight_ih_l0.data[2 * self.hidden_size: 3 * self.hidden_size, :] = self.Quantization(
-                self.weight_ih_l0_full_precision[2 * self.hidden_size: 3 * self.hidden_size, :], D_ih_l0[2 * self.hidden_size: 3 * self.hidden_size, :], method=method)
+                self.weight_ih_l0_full_precision[2 * self.hidden_size: 3 * self.hidden_size, :],
+                D_ih_l0[2 * self.hidden_size: 3 * self.hidden_size, :], method=method)
             self.weight_ih_l0.data[3 * self.hidden_size:, :] = self.Quantization(
-                self.weight_ih_l0_full_precision[3 * self.hidden_size:, :], D_ih_l0[3 * self.hidden_size:, :], method=method)
+                self.weight_ih_l0_full_precision[3 * self.hidden_size:, :], D_ih_l0[3 * self.hidden_size:, :],
+                method=method)
         self.weight_hh_l0.data[:self.hidden_size, :] = self.Quantization(
             self.weight_hh_l0_full_precision[:self.hidden_size, :], D_hh_l0[:self.hidden_size, :], method=method)
         self.weight_hh_l0.data[self.hidden_size: 2 * self.hidden_size, :] = self.Quantization(
-            self.weight_hh_l0_full_precision[self.hidden_size: 2 * self.hidden_size, :], D_hh_l0[self.hidden_size: 2 * self.hidden_size, :], method=method)
+            self.weight_hh_l0_full_precision[self.hidden_size: 2 * self.hidden_size, :],
+            D_hh_l0[self.hidden_size: 2 * self.hidden_size, :], method=method)
         self.weight_hh_l0.data[2 * self.hidden_size: 3 * self.hidden_size, :] = self.Quantization(
-            self.weight_hh_l0_full_precision[2 * self.hidden_size: 3 * self.hidden_size, :], D_hh_l0[2 * self.hidden_size: 3 * self.hidden_size, :], method=method)
+            self.weight_hh_l0_full_precision[2 * self.hidden_size: 3 * self.hidden_size, :],
+            D_hh_l0[2 * self.hidden_size: 3 * self.hidden_size, :], method=method)
         self.weight_hh_l0.data[3 * self.hidden_size:, :] = self.Quantization(
-            self.weight_hh_l0_full_precision[3 * self.hidden_size:, :], D_hh_l0[3 * self.hidden_size:, :], method=method)
+            self.weight_hh_l0_full_precision[3 * self.hidden_size:, :], D_hh_l0[3 * self.hidden_size:, :],
+            method=method)
 
     def optim_grad(self, optimizer):
-        self.weight_ih_l0_full_precision.grad=self.weight_ih_l0.grad
-        self.weight_hh_l0_full_precision.grad=self.weight_hh_l0.grad
+        self.weight_ih_l0_full_precision.grad = self.weight_ih_l0.grad
+        self.weight_hh_l0_full_precision.grad = self.weight_hh_l0.grad
+
 
 class LSTM_quantized(nn.Module):
 
-    def __init__(self, input_size, hidden_size,norm, args, num_layers=1, bias=True, dropout=0.):
+    def __init__(self, input_size, hidden_size, norm, args, num_layers=1, bias=True, dropout=0.):
         super(LSTM_quantized, self).__init__()
-        self.lstm_cell = LSTM_quantized_cell(input_size, hidden_size,norm ,args, bias)
+        self.lstm_cell = LSTM_quantized_cell(input_size, hidden_size, norm, args, bias)
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.bias = bias
@@ -358,8 +431,11 @@ class LSTM_quantized(nn.Module):
     def _forward_rnn(cell, x, hidden):
         max_time = x.shape[0]
         outputs = []
+        first = False
         for time in range(max_time):
-            _, hidden = cell(x[time], hidden, time)
+            if time == 0:
+                first = True
+            _, hidden = cell(x[time], hidden, time, first)
             outputs.append(hidden[0])
         h = hidden[0].unsqueeze(0)
         c = hidden[1].unsqueeze(0)
@@ -385,18 +461,21 @@ class LSTM_quantized(nn.Module):
     def quantize(self, optimizer):
         self.lstm_cell.quantize(optimizer)
 
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
 class mnistModel(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
 
-    def __init__(self, rnn_type,ninp, nhid, nlayers,args, quantize=False):
+    def __init__(self, rnn_type, ninp, nhid, nlayers, args, quantize=False):
         super(mnistModel, self).__init__()
         self.args = args
         self.quantize = quantize
         self.norm = args.norm
         self.rnns = [LSTM_quantized(ninp, nhid, self.norm, args, num_layers=1, dropout=0)]
         self.rnns = torch.nn.ModuleList(self.rnns)
-        self.decoder = nn.Linear(nhid, 10) # there are as a total of 10 digits
+        self.decoder = nn.Linear(nhid, 10)  # there are as a total of 10 digits
         self.init_weights()
         self.rnn_type = rnn_type
         self.ninp = ninp
@@ -404,7 +483,7 @@ class mnistModel(nn.Module):
         self.nlayers = nlayers
 
     def reset(self):
-        if self.rnn_type == 'QRNN': [r.reset() for r in self.rnns] #why we need this?
+        if self.rnn_type == 'QRNN': [r.reset() for r in self.rnns]  # why we need this?
 
     def init_weights(self):
         initrange = 0.1
