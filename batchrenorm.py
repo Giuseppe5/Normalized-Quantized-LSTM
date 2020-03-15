CONFIG: Fix for gradient for batch renorm

batchrenorm.py
@@ -4,7 +4,7 @@ import torch
 __all__ = ["BatchRenorm1d", "BatchRenorm2d", "BatchRenorm3d"]
 
 
-class BatchRenorm(torch.jit.ScriptModule):
+class BatchRenorm(torch.nn.Module):
     def __init__(
         self,
         num_features: int,
@@ -23,10 +23,10 @@ class BatchRenorm(torch.jit.ScriptModule):
             "num_batches_tracked", torch.tensor(0, dtype=torch.long)
         )
         self.weight = torch.nn.Parameter(
-            0.1 * torch.ones(num_features, dtype=torch.float)
+            torch.ones(num_features, dtype=torch.float), requires_grad=True
         )
         self.bias = torch.nn.Parameter(
-            torch.zeros(num_features, dtype=torch.float)
+            torch.zeros(num_features, dtype=torch.float), requires_grad=True
         )
         self.affine = affine
         self.eps = eps
@@ -38,50 +38,53 @@ class BatchRenorm(torch.jit.ScriptModule):
 
     @property
     def rmax(self) -> torch.Tensor:
-        return (7.428e-5 * self.num_batches_tracked + 25 / 35).clamp_(
+        return (3.714/50000 * self.num_batches_tracked + 25 / 35).clamp_(
             1.0, 3.0
         )
 
     @property
     def dmax(self) -> torch.Tensor:
-        return (8.33e-5 * self.num_batches_tracked - 25 / 20).clamp_(
+        return (6.125/75000 * self.num_batches_tracked - 25 / 20).clamp_(
             0.0, 5.0
         )
 
     def forward(self, x: torch.Tensor, first: bool) -> torch.Tensor:
-        self._check_input_dim(x)
-        if x.dim() > 2:
-            x = x.transpose(1, -1)
         if self.training:
-            dims = [i for i in range(x.dim() - 1)]
-            batch_mean = x.mean(dims)
-            batch_std = x.var(dims, unbiased=False) + self.eps
-            batch_std = 1/batch_std.pow(0.5)
-            # batch_std = x.std(dims, unbiased=False) + self.eps
+            batchsize, channels = x.size()
+            numel = batchsize
+            x = x.permute(1, 0).contiguous().view(channels, numel)
+            sum_ = x.sum(1)
+            sum_of_square = x.pow(2).sum(1)
+            mean = sum_ / numel
+            sumvar = sum_of_square - sum_ * mean
+            self.running_mean = (
+                    (1 - self.momentum) * self.running_mean
+                    + self.momentum * mean.detach())
+            unbias_var = sumvar / (numel - 1)
+            self.running_std = (
+                    (1 - self.momentum) * self.running_std
+                    + self.momentum * unbias_var.detach()
+            )
+
+            bias_var = sumvar / numel
+            inv_std = 1 / (bias_var + self.eps).pow(0.5)
             r = (
-                batch_std.detach() / self.running_std.view_as(batch_std)
+                inv_std.detach() / self.running_std.view_as(inv_std)
             ).clamp_(1 / self.rmax, self.rmax)
             d = (
-                (batch_mean.detach() - self.running_mean.view_as(batch_mean))
-                / self.running_std.view_as(batch_std)
+                (mean.detach() - self.running_mean.view_as(mean))
+                / self.running_std.view_as(inv_std)
             ).clamp_(-self.dmax, self.dmax)
 
-            x = (x - batch_mean) / batch_std * r + d
-            self.running_mean += self.momentum * (
-                batch_mean.detach() - self.running_mean
-            )
-            self.running_std += self.momentum * (
-                batch_std.detach() - self.running_std
-            )
+            x = (x - mean.unsqueeze(1)) * inv_std.unsqueeze(1) * r.unsqueeze(1) + d.unsqueeze(1)
             if first:
                 self.num_batches_tracked += 1
         else:
             x = (x - self.running_mean) / self.running_std
         if self.affine:
-            x = self.weight * x + self.bias
-        if x.dim() > 2:
-            x = x.transpose(1, -1)
-        return x
+            x = self.weight.unsqueeze(1) * x + self.bias.unsqueeze(1)
+
+        return x.view(channels, batchsize).permute(1, 0).contiguous()
 
 
 class BatchRenorm1d(BatchRenorm):

trainer.py
@@ -3,9 +3,10 @@ import os
 import time
 import io
 
+
 class Trainer(object):
 
-    def __init__(self, optimizer, criterion, params_invariant ,args, ):
+    def __init__(self, optimizer, criterion, params_invariant, args, ):
         self._optimizer = optimizer
         self._epoch = 1
         self.args = args
@@ -24,11 +25,11 @@ class Trainer(object):
 
     def save_cktpt(self, model, fn):
         state_dict = {
-            'optimizer_state' : self._optimizer.state_dict(),
-            'epoch' : self._epoch,
-            'model' : model.state_dict(),
-            'validloss' : self.validloss,
-            'testloss' : self.testloss,
+            'optimizer_state': self._optimizer.state_dict(),
+            'epoch': self._epoch,
+            'model': model.state_dict(),
+            'validloss': self.validloss,
+            'testloss': self.testloss,
         }
         torch.save(state_dict, fn)
         print('save checkpint for epoch {}'.format(self._epoch))
@@ -51,10 +52,10 @@ class Trainer(object):
         model.eval()
         total_loss = 0
         true_case = 0
-        total_case =0
+        total_case = 0
         for i, sample in enumerate(loader):
             images, labels = sample
-            images = images.transpose(0, 1) # seq length, bsz, input = 784, 100, 1
+            images = images.transpose(0, 1)  # seq length, bsz, input = 784, 100, 1
             images = images.cuda()
             labels = labels.cuda()
             output = model(images, self._optimizer)
@@ -67,7 +68,7 @@ class Trainer(object):
                 equal_case = torch.eq(argmax, labels)
                 true_case += int(equal_case.float().sum().item())
         if test:
-            return (total_loss / total_case , float(true_case) / total_case)
+            return (total_loss / total_case, float(true_case) / total_case)
         else:
             return total_loss / total_case
 
@@ -92,9 +93,13 @@ class Trainer(object):
             torch.nn.utils.clip_grad_norm_(self.params_invariant, self.args.clip)
 
             self._optimizer.step()
-            if j % 10 == 0 :
+            if j % 10 == 0:
                 elapsed = time.time() - start_time
-                print('{:03d} loss {:8.6f} elaped time {:6.3f}  learning rate {:08.6f}'.format(j * self.args.batchsize, raw_loss.item(), elapsed, self._optimizer.param_groups[0]['lr']))
+                print('{:03d} loss {:8.6f} elaped time {:6.3f}  learning rate {:08.6f} r {:2.2f} d {:2.2f}'.format(j * self.args.batchsize,
+                                                                                               raw_loss.item(), elapsed,
+                                                                                               self._optimizer.param_groups[0]['lr'],
+                                                                                               model.rnns._modules['0']._modules['cell'].bn_h.dmax,
+                                                                                               model.rnns._modules['0']._modules['cell'].bn_h.rmax))
                 start_time = time.time()
         return total_loss / total_cases
 
@@ -105,19 +110,19 @@ class Trainer(object):
             print('| End of epoch {:3d} | acc {:6.3f} | test loss {:6.3f} |'.format(self._epoch - 1, acc,
                                                                                     test_loss, ))
             print('=' * 89)
-        for epoch in range(self._epoch, self.args.epochs+1):
+        for epoch in range(self._epoch, self.args.epochs + 1):
             epoch_start_time = time.time()
             trainloss = self.train_step(model, trainloader)
             val_loss = self.evaluate_step(model, validloader)
             print('-' * 89)
             print('| end of epoch {:3d} | time: {:6.3f}s | valid loss {:6.3f} | '.format(
-              epoch, (time.time() - epoch_start_time), val_loss, ))
+                epoch, (time.time() - epoch_start_time), val_loss, ))
             print('-' * 89)
             # Run on test data.
             test_loss, acc = self.evaluate_step(model, testloader, test=True)
             print('=' * 89)
             print('| End of epoch {:3d} | acc {:6.3f} | test loss {:6.3f} |'.format(epoch, acc,
-                test_loss, ))
+                                                                                    test_loss, ))
             print('=' * 89)
 
             if val_loss < self.validloss:
@@ -127,8 +132,8 @@ class Trainer(object):
             else:
                 is_best = False
             if self.args.optimizer == 'adam':
-                    self._optimizer.param_groups[0]['lr'] *= self.args.lr_decay
-            with io.open(self.args.save +  '.log', 'a', newline='\n', encoding='utf8', errors='ignore') as tgt:
+                self._optimizer.param_groups[0]['lr'] *= self.args.lr_decay
+            with io.open(self.args.save + '.log', 'a', newline='\n', encoding='utf8', errors='ignore') as tgt:
                 msg = 'epoch {:03d} train_loss {:010.7f} valid_loss {:010.7f} test_loss {:010.7f} accuracy {:06.4f}\n'.format(
                     self._epoch, trainloss, val_loss, test_loss, acc
                 )